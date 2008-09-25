bc6debaa4e1736d0bb4ab75265eda263ab4b46a0
Martin Decky <martin@decky.cz>
2008-09-25 12:20:27 +0000

    cleanup

--- src/endi.h
+++ src/endi.h
@@ -1,16 +1,16 @@
 /*
- * endi.h
- * checking endian
  * Copyright (c) 2001-2005 Viliam Holub
+ * All rights reserved.
+ *
+ * Distributed under the terms of GPL.
+ *
+ *
+ *  Endianess conversion
+ *
  */
 
-
-#ifndef _ENDI_H_
-#define _ENDI_H_
-
-#ifdef HAVE_CONFIG_H
-#	include "../config.h"
-#endif
+#ifndef ENDI_H_
+#define ENDI_H_
 
 #include <stdbool.h>
 
@@ -18,20 +18,20 @@
 
 extern bool little_endian;
 
-void checkendian( void);
-bool test_type( void);
+extern void checkendian(void);
+extern bool test_type(void);
 
 #ifdef WORDS_BIGENDIAN
-# define convert_uint8_t_endian( b)	(b)
-# define convert_uint16_t_endian( h)	\
-		( (((h) << 8) & 0xff00U) | (((h) >> 8) & 0x00ffU) )
-# define convert_uint32_t_endian( w)	\
-		( (((w)&0xffU)<<24) | (((w)&0xff00U)<<8) | \
-		(((w)&0xff0000U)>>8) | (((w)&0xff000000U)>>24) );
+	#define convert_uint8_t_endian(b) (b)
+	#define convert_uint16_t_endian(h) \
+		((((h) << 8) & 0xff00U) | (((h) >> 8) & 0x00ffU))
+	#define convert_uint32_t_endian(w)	\
+		((((w) & 0xffU) << 24) | (((w) & 0xff00U) << 8) | \
+		(((w) & 0xff0000U) >> 8) | (((w) & 0xff000000U) >> 24))
 #else
-# define convert_uint8_t_endian( b)	(b)
-# define convert_uint16_t_endian( h)	(h)
-# define convert_uint32_t_endian( w)	(w)
+	#define convert_uint8_t_endian(b) (b)
+	#define convert_uint16_t_endian(h) (h)
+	#define convert_uint32_t_endian(w) (w)
 #endif
 
-#endif /* _ENDI_H_ */
+#endif /* ENDI_H_ */

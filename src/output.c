6c8098ead8bd3fb6ffb1b088142e8d98c5c2374d
Martin Decky <martin@decky.cz>
2008-09-25 15:23:56 +0000

    cleanup

--- src/output.c
+++ src/output.c
@@ -1,27 +1,28 @@
 /*
- * MSIM Output management
- * 
  * Copyright (c) 2003-2005 Viliam Holub
+ * All rights reserved.
+ *
+ * Distributed under the terms of GPL.
  *
  *
+ *  Output routines
+ *
+ */
+
+/*
  * We must take a special care with output messages. Some types of messages
  * often exceeds the with of the screen and just simple splitting to more
  * lines may cause unexpected output format.
  *
  * Thus we provide a set of output functions which behaviour depends on
- * the type of information they belongs to.
+ * the type of information they belong to.
  *
  * Regular text messages are buffered and lines are splitted at word
  * boundaries.
  *
  * This approach works fine in most cases.
  *
- * XXX
  */
-
-#ifdef HAVE_CONFIG_H
-#	include "../config.h"
-#endif
 
 #include <stdarg.h>
 #include <unistd.h>
@@ -36,245 +37,229 @@
 #include "parser.h"
 #include "cline.h"
 
-/*
- * The output buffer size
- */
-#define OUTPUT_BUF_SIZE	256
-#define DBUF_SIZE	4096
+
+/**< The output buffer size */
+#define OUTPUT_BUF_SIZE 256
+#define DBUF_SIZE       4096
+#define BUF_SIZ         256
 
 
-/*
- * Output file descriptor.
- */
+/**< Output file descriptor */
 FILE *output;
 
 
 volatile int screen_width;
 int buf_end;
-char output_buf[ OUTPUT_BUF_SIZE];
+char output_buf[OUTPUT_BUF_SIZE];
 
 int scr_cur;
 
 
-/** output_init - Initializes the output management.
+/** Initialize the output management
+ *
  */
-void
-output_init( void)
-	
+void output_init(void)
 {
 	output = stdout;
 	buf_end = 0;
 	scr_cur = 0;
 	
-	if (isatty( 1))
-		screen_width = 79; /* ? */
+	if (isatty(1))
+		screen_width = 79;
 	else
 		screen_width = 79;
 }
 
 
-
-/** Prints the message on the screen.
+/** Print the message on the screen
+ *
  */
-void
-mprintf( const char *fmt, ...)
-
+void mprintf(const char *fmt, ...)
 {
 	va_list ap;
-	char dbuf[ DBUF_SIZE];
+	char dbuf[DBUF_SIZE];
 
-	PRE( fmt != NULL);
+	PRE(fmt != NULL);
 
-	va_start( ap, fmt);
-	vsnprintf( dbuf, DBUF_SIZE, fmt, ap);
-	va_end( ap);
+	va_start(ap, fmt);
+	vsnprintf(dbuf, DBUF_SIZE, fmt, ap);
+	va_end(ap);
 
-	fprintf( output, "%s", dbuf);
+	fprintf(output, "%s", dbuf);
 	scr_cur = 0;
 }
 
 
-/** Prints the message on the screen.
+/** Print the message on the screen
  *
- * The cursor pointer is not modified.
+ * The cursor pointer is not modified
+ *
  */
-void
-mprintf2( const char *fmt, ...)
-
+void mprintf2(const char *fmt, ...)
 {
 	va_list ap;
-	char dbuf[ DBUF_SIZE];
+	char dbuf[DBUF_SIZE];
 
-	PRE( fmt != NULL);
+	PRE(fmt != NULL);
 
-	va_start( ap, fmt);
-	vsnprintf( dbuf, DBUF_SIZE, fmt, ap);
-	va_end( ap);
+	va_start(ap, fmt);
+	vsnprintf(dbuf, DBUF_SIZE, fmt, ap);
+	va_end(ap);
 
-	fprintf( output, "%s", dbuf);
+	fprintf(output, "%s", dbuf);
 }
 
 
-/** Counts a string space occupation on the screen.
+/** Count a string space occupation on the screen
  *
  * The only interesting character is '\t' which has to be counted carefully.
+ *
  */
-int
-string_space( const char *str)
-
+int string_space(const char *str)
 {
 	int r;
 
-	PRE( str != NULL);
+	PRE(str != NULL);
 
-	for (r=0; *str; r++, str++)
+	for (r = 0; *str; r++, str++)
 		if (*str == '\t')
-			r += 8-r%8 -1;
-
+			r += 8 - r % 8 - 1;
+	
 	return r;
 }
 
 
-/** Prints the message with breaking tags. 
+/** Print the message with breaking tag.
  *
- * The function prints the formatted text specified. Additionally, it breaks
+ * Print the formatted text specified. Additionally, it break
  * the line on a special character BTAG, if the text could exceed the screen
  * width. On a new line, string "nl" is printed.
  *
  * There should not be the end-of-line '\n' character except the end of fmt
  * string.
  *
- * nl	string which will be displayed on new lines
- * fmt	a printf-like text format
+ * @param nl  String which will be displayed on new lines
+ * @param fmt A printf-like text format
+ *
  */
-void
-mprintf_btag( const char *nl, const char *fmt, ...)
-
+void mprintf_btag(const char *nl, const char *fmt, ...)
 {
 	va_list ap;
-	char dbuf[ DBUF_SIZE];
+	char dbuf[DBUF_SIZE];
 	int ncur, acur;
 	char *s;
 	char x;
 	int sw = screen_width;
 	bool next_line = false;
 
-	PRE( fmt != NULL);
+	PRE(fmt != NULL);
 
 	if (!nl)
 		nl = "";
-	ncur = string_space( nl);
+	ncur = string_space(nl);
 
-	va_start( ap, fmt);
-	vsnprintf( dbuf, DBUF_SIZE, fmt, ap);
-	va_end( ap);
+	va_start(ap, fmt);
+	vsnprintf(dbuf, DBUF_SIZE, fmt, ap);
+	va_end(ap);
 
-	// line break test
-	if (scr_cur > sw-1)
-	{
-		mprintf2( "\n%s", nl);
+	/* Line break test */
+	if (scr_cur > sw - 1) {
+		mprintf2("\n%s", nl);
 		scr_cur = ncur;
 		next_line = true;
 	}
 
 	s = dbuf;
-	for (x=*s; x; s++)
-	{
-		// remove TBRK prefix
-		while (*s && *s == TBRK_C)
+	for (x = *s; x; s++) {
+		/* Remove TBRK prefix */
+		while ((*s) && (*s == TBRK_C))
 			s++;
+		
+		/* Keep the substring start */
+		char *so = s; 
 
-		char *so = s; // keeps the substring start
+		/* Find the end of the substring */
+		for (; (*s) && (*s != TBRK_C); s++);
 
-		// find the end of the substring
-		for (; *s && *s!=TBRK_C; s++) ;
+		/* Save the "type" of the end
+		   set the string terminator */
+		x = *s;
+		*s = 0;
 
-		// save the "type" of the end, set the string terminator
-		x = *s; *s = 0;
-
-		// calculate the substring length
-		acur = string_space( so);
+		/* Calculate the substring length */
+		acur = string_space(so);
 
 		if (acur == 0)
 			continue;
 
 		scr_cur += acur;
 
-		// line break test
-		if (scr_cur > sw-1 && !next_line)
-		{
-			mprintf2( "\n%s", nl);
-			scr_cur = ncur +acur;
+		/* Line break test */
+		if ((scr_cur > sw - 1) && (!next_line)) {
+			mprintf2("\n%s", nl);
+			scr_cur = ncur + acur;
 			next_line = true;
 		}
 		next_line = false;
 		
-		// print the substring
+		/* Print the substring */
 		if (x)
-			mprintf2( "%s", so);
-		else
-		{
-			mprintf2( "%s", so);
+			mprintf2("%s", so);
+		else {
+			mprintf2("%s", so);
 
-			if (*(s-1) == '\n')
+			if (*(s - 1) == '\n')
 				scr_cur = 0;
 		}
-
 	}
 }
 
 
-/** Aka mprintf but with limited output length.
+/** Aka mprintf but with limited output length
  *
  * Maximum string length is 256.
+ *
  */
-void
-mprintf_n( int n, const char *fmt, ...)
-
+void mprintf_n(int n, const char *fmt, ...)
 {
-	char buf[ 256];
+	char buf[256];
 	va_list ap;
 
-	PRE( fmt != NULL);
+	PRE(fmt != NULL);
 
 	if (n > 255)
 		n = 255;
 	
-	va_start( ap, fmt);
-	vsnprintf( buf, n+1, fmt, ap);
-	va_end( ap);
+	va_start(ap, fmt);
+	vsnprintf(buf, n + 1, fmt, ap);
+	va_end(ap);
 
-	fprintf( output, "%s", buf);
+	fprintf(output, "%s", buf);
 	scr_cur = 0;
 }
 
 
-#define BUF_SIZ	256
-
-/** Prints the text.
+/** Print the text
+ *
  */
-void
-mprintf_text( const char *nl, const char *fmt, ...)
-
+void mprintf_text(const char *nl, const char *fmt, ...)
 {
-	char buf[ BUF_SIZ];
-	char buf2[ 2*BUF_SIZ];
+	char buf[BUF_SIZ];
+	char buf2[2 * BUF_SIZ];
 	va_list ap;
 	char *s, *s2;
 
-	PRE( fmt != NULL);
+	PRE(fmt != NULL);
 
-	va_start( ap, fmt);
-	vsnprintf( buf, BUF_SIZ, fmt, ap);
-	va_end( ap);
+	va_start(ap, fmt);
+	vsnprintf(buf, BUF_SIZ, fmt, ap);
+	va_end(ap);
 
 	s = buf;
 	s2 = buf2;
 	
-	for (; *s; s++, s2++)
-	{
-		if (*s == ' ' || *s == '\n')
-		{
+	for (; *s; s++, s2++) {
+		if ((*s == ' ') || (*s == '\n')) {
 			*s2 = *s;
 			s2++;
 			*s2 = TBRK_C;
@@ -285,31 +270,30 @@ mprintf_text( const char *nl, const char *fmt, ...)
 	}
 	*s2 = '\0';
 
-	mprintf_btag( nl, "%s", buf2);
+	mprintf_btag(nl, "%s", buf2);
 }
 
 
-/** Prints the error message
+/** Print the error message
+ *
  */
-void
-mprintf_err( const char *fmt, ...)
-
+void mprintf_err(const char *fmt, ...)
 {
-	char buf[ BUF_SIZ];
+	char buf[BUF_SIZ];
 	va_list ap;
 	size_t len;
 
-	PRE( fmt != NULL);
+	PRE(fmt != NULL);
 
 	if (lineno != -1)
-		len = sprintf( buf, "%d: ", lineno);
+		len = sprintf(buf, "%d: ", lineno);
 	else
 		len = 0;
-	buf[ len] = '\0';
+	buf[len] = '\0';
 
-	va_start( ap, fmt);
-	vsnprintf( buf+len, BUF_SIZ-len, fmt, ap);
-	va_end( ap);
+	va_start(ap, fmt);
+	vsnprintf(buf + len, BUF_SIZ - len, fmt, ap);
+	va_end(ap);
 
 	mprintf_text( NULL, "%s", buf);
 }
--- src/output.h
+++ src/output.h
@@ -1,19 +1,25 @@
 /*
- * Copyright (c) 2001-2004 Viliam Holub
+ * Copyright (c) 2003-2005 Viliam Holub
+ * All rights reserved.
+ *
+ * Distributed under the terms of GPL.
+ *
+ *
+ *  Output routines
+ *
  */
 
+#ifndef OUTPUT_H_
+#define OUTPUT_H_
 
-#ifndef _OUTPUT_H_
-#define _OUTPUT_H_
+#define TBRK   "\001"
+#define TBRK_C '\001'
 
-#define TBRK	"\001"
-#define TBRK_C	'\001'
+extern void output_init(void);
+extern void mprintf(const char *fmt, ...);
+extern void mprintf_btag(const char *nl, const char *fmt, ...);
+extern void mprintf_n(int n, const char *fmt, ...);
+extern void mprintf_text(const char *in, const char *fmt, ...);
+extern void mprintf_err(const char *fmt, ...);
 
-void output_init( void);
-void mprintf( const char *fmt, ...);
-void mprintf_btag( const char *nl, const char *fmt, ...);
-void mprintf_n( int n, const char *fmt, ...);
-void mprintf_text( const char *in, const char *fmt, ...);
-void mprintf_err( const char *fmt, ...);
-
-#endif /* _OUTPUT_H_ */
+#endif /* OUTPUT_H_ */

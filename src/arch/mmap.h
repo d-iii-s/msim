49fb0c854795893ded4179288b1cf2a79882afd3
Martin Decky <martin@decky.cz>
2008-10-25 15:02:57 +0000

    add defines

--- src/arch/mmap.h
+++ src/arch/mmap.h
@@ -11,7 +11,15 @@
 
 #ifdef __WIN32__
 
-#define MAP_FAILED  ((void *) -1)
+#define MAP_FAILED   ((void *) -1)
+
+#define PROT_READ    0x01
+#define PROT_WRITE   0x02
+#define PROT_EXEC    0x04
+#define PROT_NONE    0x00
+
+#define MAP_SHARED   0x01
+#define MAP_PRIVATE  0x02
 
 extern void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
 extern int munmap(void *addr, size_t length);

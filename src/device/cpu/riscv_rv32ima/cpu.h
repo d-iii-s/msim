149a7e29f0de215b180a3e5311eb54dcc1ad2321
Jan Papesch <janpapuch@seznam.cz>
2022-04-05 19:51:58 +0200

    basic loads supported

--- src/device/cpu/riscv_rv32ima/cpu.h
+++ src/device/cpu/riscv_rv32ima/cpu.h
@@ -17,6 +17,7 @@
 #include <stdbool.h>
 #include "instr.h"
 #include "csr.h"
+#include "../../../main.h"
 
 #define RV32IMA_REG_COUNT	32
 
@@ -83,5 +84,10 @@ extern void rv_cpu_step(rv_cpu_t *cpu);
 /** Interrupts */
 extern void rv_cpu_interrupt_up(rv_cpu_t *cpu, unsigned int no);
 extern void rv_cpu_interrupt_down(rv_cpu_t *cpu, unsigned int no);
+
+/** Memory operations */
+extern rv_exc_t rv_convert_addr(rv_cpu_t *cpu, uint32_t virt, ptr36_t *phys, bool wr, bool noisy);
+extern rv_exc_t rv_read_mem32(rv_cpu_t *cpu, uint32_t virt, uint32_t *value, bool noisy);
+
 
 #endif //RISCV_RV32IMA_CPU_H_

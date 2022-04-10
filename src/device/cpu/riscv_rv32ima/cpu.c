5f33970642663fa277bc10d242136d8c0a2aa927
Jan Papesch <janpapuch@seznam.cz>
2022-04-10 15:57:58 +0200

    prepared rv_cpu_t for jumps

--- src/device/cpu/riscv_rv32ima/cpu.c
+++ src/device/cpu/riscv_rv32ima/cpu.c
@@ -14,6 +14,7 @@ static void init_regs(rv_cpu_t *cpu) {
     // expects that default value for any variable is 0
 
     cpu->pc = RV_START_ADDRESS;
+    cpu->pc_next = RV_START_ADDRESS + 4;
 }
 
 
@@ -43,6 +44,7 @@ rv_exc_t rv_convert_addr(rv_cpu_t *cpu, uint32_t virt, ptr36_t *phys, bool wr, b
 rv_exc_t rv_read_mem32(rv_cpu_t *cpu, uint32_t virt, uint32_t *value, bool noisy){
     ASSERT(cpu != NULL);
     ASSERT(value != NULL);
+    //TODO: check alignment
 
     ptr36_t phys;
     rv_exc_t ex = rv_convert_addr(cpu, virt, &phys, false, noisy);
@@ -57,6 +59,7 @@ rv_exc_t rv_read_mem32(rv_cpu_t *cpu, uint32_t virt, uint32_t *value, bool noisy
 
 rv_exc_t rv_write_mem32(rv_cpu_t *cpu, uint32_t virt, uint32_t value, bool noisy){
     ASSERT(cpu != NULL);
+    // TODO: check alignment
 
     ptr36_t phys;
     rv_exc_t ex = rv_convert_addr(cpu, virt, &phys, false, noisy);
@@ -97,25 +100,28 @@ void rv_cpu_set_pc(rv_cpu_t *cpu, uint32_t value){
 
 void rv_cpu_step(rv_cpu_t *cpu){
 
-    //TODO: pc is virtual
+    ptr36_t phys;
+    rv_exc_t ex;
+    while((ex = rv_convert_addr(cpu, cpu->pc, &phys, false, true)) != rv_exc_none){
+        // TODO: handle exception
+    }
 
-    uint32_t val = physmem_read32(cpu->csr.mhartid, cpu->pc, false);
+    rv_instr_t instr_data = (rv_instr_t)physmem_read32(cpu->csr.mhartid, phys, false);
 
-    printf("Cycle: %ld\tpc: 0x%08x\tmem: 0x%08x ", cpu->csr.cycle, cpu->pc, val);
+    printf("Cycle: %ld\tpc: 0x%08x\tmem: 0x%08x ", cpu->csr.cycle, cpu->pc, instr_data.val);
     
-    rv_instr_func_t func = rv_instr_decode((rv_instr_t)val);
-    func(cpu, (rv_instr_t)val);
+    rv_instr_func_t instr_func = rv_instr_decode(instr_data);
+    instr_func(cpu, instr_data);
 
     printf("\n");
 
     cpu->csr.cycle++;
-    cpu->pc += 4;
+
     // x0 is always 0
     cpu->regs[0] = 0;
-
-    if(val == UINT32_C(-1)){
-        die(120, "reached end of memory!");
-    }
+    cpu->pc = cpu->pc_next;
+    cpu->pc_next = cpu->pc + 4;
+    
 }
 
 /** Interrupts */

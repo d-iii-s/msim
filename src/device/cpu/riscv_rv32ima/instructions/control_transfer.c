39e6315b6496347315fc81fc4ad54ffcecddc0eb
Jan Papesch <janpapuch@seznam.cz>
2022-04-11 11:47:16 +0200

    branch tests added

--- src/device/cpu/riscv_rv32ima/instructions/control_transfer.c
+++ src/device/cpu/riscv_rv32ima/instructions/control_transfer.c
@@ -43,20 +43,139 @@ rv_exc_t jalr_instr(rv_cpu_t *cpu, rv_instr_t instr) {
 }
 
 rv_exc_t beq_instr(rv_cpu_t *cpu, rv_instr_t instr){
-    return rv_exc_illegal_instruction;
+    ASSERT(cpu != NULL);
+    ASSERT(instr.b.opcode == rv_opcBRANCH);
+
+    // target is relative to address of the instruction
+    uint32_t target = cpu->pc + RV_B_IMM(instr);
+
+    if(!IS_ALIGNED(target, 4)){
+        return rv_exc_instruction_address_misaligned;
+    }
+
+    uint32_t lhs = cpu->regs[instr.b.rs1];
+    uint32_t rhs = cpu->regs[instr.b.rs2];
+
+    printf("BEQ branch to %08x condition %d == %d %s", target, lhs, rhs, (lhs == rhs ? "taken" : "not taken"));
+
+    if(lhs == rhs) {
+        cpu->pc_next = target;
+    }
+
+    return rv_exc_none;
 }
+
 rv_exc_t bne_instr(rv_cpu_t *cpu, rv_instr_t instr){
-    return rv_exc_illegal_instruction;
+    ASSERT(cpu != NULL);
+    ASSERT(instr.b.opcode == rv_opcBRANCH);
+
+    // target is relative to address of the instruction
+    uint32_t target = cpu->pc + RV_B_IMM(instr);
+
+    if(!IS_ALIGNED(target, 4)){
+        return rv_exc_instruction_address_misaligned;
+    }
+
+    uint32_t lhs = cpu->regs[instr.b.rs1];
+    uint32_t rhs = cpu->regs[instr.b.rs2];
+
+    printf("BNE branch to %08x condition %d != %d %s", target, lhs, rhs, (lhs != rhs ? "taken" : "not taken"));
+
+    if(lhs != rhs) {
+        cpu->pc_next = target;
+    }
+
+    return rv_exc_none;
 }
+
 rv_exc_t blt_instr(rv_cpu_t *cpu, rv_instr_t instr){
-    return rv_exc_illegal_instruction;
+    ASSERT(cpu != NULL);
+    ASSERT(instr.b.opcode == rv_opcBRANCH);
+
+    // target is relative to address of the instruction
+    uint32_t target = cpu->pc + RV_B_IMM(instr);
+
+    if(!IS_ALIGNED(target, 4)){
+        return rv_exc_instruction_address_misaligned;
+    }
+
+    int32_t lhs = (int32_t)cpu->regs[instr.b.rs1];
+    int32_t rhs = (int32_t)cpu->regs[instr.b.rs2];
+
+    printf("BLT branch to %08x condition %d < %d %s", target, lhs, rhs, (lhs < rhs ? "taken" : "not taken"));
+
+    if(lhs < rhs) {
+        cpu->pc_next = target;
+    }
+
+    return rv_exc_none;
 }
+
 rv_exc_t bltu_instr(rv_cpu_t *cpu, rv_instr_t instr){
-    return rv_exc_illegal_instruction;
+    ASSERT(cpu != NULL);
+    ASSERT(instr.b.opcode == rv_opcBRANCH);
+
+    // target is relative to address of the instruction
+    uint32_t target = cpu->pc + RV_B_IMM(instr);
+
+    if(!IS_ALIGNED(target, 4)){
+        return rv_exc_instruction_address_misaligned;
+    }
+
+    uint32_t lhs = cpu->regs[instr.b.rs1];
+    uint32_t rhs = cpu->regs[instr.b.rs2];
+
+    printf("BLTU branch to %08x condition %u < %u %s", target, lhs, rhs, (lhs < rhs ? "taken" : "not taken"));
+
+    if(lhs < rhs) {
+        cpu->pc_next = target;
+    }
+
+    return rv_exc_none;
 }
+
 rv_exc_t bge_instr(rv_cpu_t *cpu, rv_instr_t instr){
-    return rv_exc_illegal_instruction;
+    ASSERT(cpu != NULL);
+    ASSERT(instr.b.opcode == rv_opcBRANCH);
+
+    // target is relative to address of the instruction
+    uint32_t target = cpu->pc + RV_B_IMM(instr);
+
+    if(!IS_ALIGNED(target, 4)){
+        return rv_exc_instruction_address_misaligned;
+    }
+
+    int32_t lhs = (int32_t)cpu->regs[instr.b.rs1];
+    int32_t rhs = (int32_t)cpu->regs[instr.b.rs2];
+
+    printf("BGE branch to %08x condition %d >= %d %s", target, lhs, rhs, (lhs >= rhs ? "taken" : "not taken"));
+
+    if(lhs >= rhs) {
+        cpu->pc_next = target;
+    }
+
+    return rv_exc_none;
 }
+
 rv_exc_t bgeu_instr(rv_cpu_t *cpu, rv_instr_t instr){
-    return rv_exc_illegal_instruction;
+    ASSERT(cpu != NULL);
+    ASSERT(instr.b.opcode == rv_opcBRANCH);
+
+    // target is relative to address of the instruction
+    uint32_t target = cpu->pc + RV_B_IMM(instr);
+
+    if(!IS_ALIGNED(target, 4)){
+        return rv_exc_instruction_address_misaligned;
+    }
+
+    uint32_t lhs = cpu->regs[instr.b.rs1];
+    uint32_t rhs = cpu->regs[instr.b.rs2];
+
+    printf("BGU branch to %08x condition %u >= %u %s", target, lhs, rhs, (lhs >= rhs ? "taken" : "not taken"));
+
+    if(lhs >= rhs) {
+        cpu->pc_next = target;
+    }
+
+    return rv_exc_none;
 }

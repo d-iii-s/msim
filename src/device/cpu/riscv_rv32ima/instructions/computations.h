8e3dc54e7c626406e229f3f9cbb86358a5c763a2
Jan Papesch <janpapuch@seznam.cz>
2022-04-22 17:58:53 +0200

    added immediate skeleton

--- src/device/cpu/riscv_rv32ima/instructions/computations.h
+++ src/device/cpu/riscv_rv32ima/instructions/computations.h
@@ -15,4 +15,18 @@ extern rv_exc_t sra_instr(rv_cpu_t *cpu, rv_instr_t instr);
 extern rv_exc_t or_instr(rv_cpu_t *cpu, rv_instr_t instr);
 extern rv_exc_t and_instr(rv_cpu_t *cpu, rv_instr_t instr);
 
+extern rv_exc_t addi_instr(rv_cpu_t *cpu, rv_instr_t instr);
+extern rv_exc_t slti_instr(rv_cpu_t *cpu, rv_instr_t instr);
+extern rv_exc_t sltiu_instr(rv_cpu_t *cpu, rv_instr_t instr);
+extern rv_exc_t andi_instr(rv_cpu_t *cpu, rv_instr_t instr);
+extern rv_exc_t ori_instr(rv_cpu_t *cpu, rv_instr_t instr);
+extern rv_exc_t xori_instr(rv_cpu_t *cpu, rv_instr_t instr);
+extern rv_exc_t slli_instr(rv_cpu_t *cpu, rv_instr_t instr);
+extern rv_exc_t srli_instr(rv_cpu_t *cpu, rv_instr_t instr);
+extern rv_exc_t srai_instr(rv_cpu_t *cpu, rv_instr_t instr);
+
+extern rv_exc_t lui_instr(rv_cpu_t *cpu, rv_instr_t instr);
+extern rv_exc_t auipc_instr(rv_cpu_t *cpu, rv_instr_t instr);
+
+
 #endif // RISCV_RV32IMA_INSTR_COMPUTATIONS_H_

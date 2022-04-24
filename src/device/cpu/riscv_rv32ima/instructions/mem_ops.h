fa09662aebc3b0e43ba7001525a8d6cc9b60cc44
Jan Papesch <janpapuch@seznam.cz>
2022-04-24 14:03:45 +0200

    added FENCE instr

--- src/device/cpu/riscv_rv32ima/instructions/mem_ops.h
+++ src/device/cpu/riscv_rv32ima/instructions/mem_ops.h
@@ -7,4 +7,6 @@
 extern rv_exc_t load_instr(rv_cpu_t *cpu, rv_instr_t instr);
 extern rv_exc_t store_instr(rv_cpu_t *cpu, rv_instr_t instr);
 
+extern rv_exc_t fence_instr(rv_cpu_t *cpu, rv_instr_t instr);
+
 #endif // RISCV_RV32IMA_INSTR_MEM_OPS_H_

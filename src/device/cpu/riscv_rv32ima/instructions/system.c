27a8294c1b5d44bb85cb6c2fa759b86d3e977d5d
Jan Papesch <janpapuch@seznam.cz>
2022-04-26 22:01:15 +0200

    changed ebreak and ehalt messages

--- src/device/cpu/riscv_rv32ima/instructions/system.c
+++ src/device/cpu/riscv_rv32ima/instructions/system.c
@@ -6,9 +6,7 @@ rv_exc_t break_instr(rv_cpu_t *cpu, rv_instr_t instr){
     ASSERT(cpu != NULL);
     ASSERT(instr.i.opcode == rv_opcSYSTEM);
 
-    printf(" [BREAK]\n");
-
-    alert("BREAK");
+    alert("EBREAK: breakpoint reached, entering interactive mode");
 
     machine_interactive = true;
     return rv_exc_none;
@@ -16,8 +14,8 @@ rv_exc_t break_instr(rv_cpu_t *cpu, rv_instr_t instr){
 rv_exc_t halt_instr(rv_cpu_t *cpu, rv_instr_t instr){
     ASSERT(cpu != NULL);
     ASSERT(instr.i.opcode == rv_opcSYSTEM);
-    printf(" [HALT]\n");
-    alert("HALT");
+
+    alert("EHALT: Machine halt");
 
     machine_halt = true;
     return rv_exc_none;

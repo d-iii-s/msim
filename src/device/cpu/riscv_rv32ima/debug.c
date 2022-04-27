f11470e7839ce396d65e0bf2d95349c2f0bf1217
Jan Papesch <janpapuch@seznam.cz>
2022-04-27 15:34:12 +0200

    left justify mnemonicw

--- src/device/cpu/riscv_rv32ima/debug.c
+++ src/device/cpu/riscv_rv32ima/debug.c
@@ -95,7 +95,7 @@ void rv_idump(rv_cpu_t *cpu, uint32_t addr, rv_instr_t instr){
     if(iopc)        
         printf("%-8s ", s_opc.str);
     
-    printf("%20s", s_mnemonics.str);
+    printf("%-20s", s_mnemonics.str);
 
     if(icmt && s_comments.size > 0 && s_comments.str[0] != 0){
         printf("    [ %s ]", s_comments.str);

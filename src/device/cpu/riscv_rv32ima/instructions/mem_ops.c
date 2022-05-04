b17c576c4b0c036f371b71e57666d811452b1d48
Jan Papesch <janpapuch@seznam.cz>
2022-05-04 16:05:16 +0200

    added lr and cs implementation

--- src/device/cpu/riscv_rv32ima/instructions/mem_ops.c
+++ src/device/cpu/riscv_rv32ima/instructions/mem_ops.c
@@ -2,6 +2,8 @@
 
 #include "mem_ops.h"
 #include "../../../../assert.h"
+#include "../../../../physmem.h"
+#include "../../../../fault.h"
 
 rv_exc_t lb_instr(rv_cpu_t *cpu, rv_instr_t instr){
     ASSERT(cpu != NULL);
@@ -153,12 +155,89 @@ rv_exc_t fence_instr(rv_cpu_t *cpu, rv_instr_t instr){
 /* A extension LR and SC */
 
 rv_exc_t lr_instr(rv_cpu_t *cpu, rv_instr_t instr){
+    ASSERT(cpu != NULL);
+    ASSERT(instr.r.opcode == rv_opcAMO);
+    ASSERT(instr.r.rs2 == 0);
+
+    uint32_t virt = cpu->regs[instr.r.rs1];
+
+    //TODO: handle unaligned virt
+
+    uint32_t val;
+    rv_exc_t ex = rv_read_mem32(cpu, virt, &val, true);
+
+    if(ex != rv_exc_none){
+        // if read failed, cancel all previous reservations
+        sc_unregister(cpu->csr.mhartid);
+        cpu->reserved_valid = false;
+        return ex;
+    }
+
+    // store the read value
+    cpu->regs[instr.r.rd] = val;
+
+    // we track physical addresses, so convert
+    // this should not fail
+
+    ptr36_t phys;
+    ex = rv_convert_addr(cpu, virt, &phys, false, false);
+    ASSERT(ex == rv_exc_none);
+
+    // register address for tracking
+
+    sc_register(cpu->csr.mhartid);
+    cpu->reserved_valid = true;
+    cpu->reserved_addr = phys;
 
     return rv_exc_none;
 }
 
 rv_exc_t sc_instr(rv_cpu_t *cpu, rv_instr_t instr){
+    ASSERT(cpu != NULL);
+    ASSERT(instr.r.opcode == rv_opcAMO);
 
+    if(cpu->reserved_valid == false){
+        // reservation is not valid
+        cpu->regs[instr.r.rs2] = 1;
+        return rv_exc_none;
+    }
+
+    // SC always invalidates reservation by this hart
+    cpu->reserved_valid = false;
+    sc_unregister(cpu->csr.mhartid);
+
+    // convert addr and check if the target is tracked
+    uint32_t virt = cpu->regs[instr.r.rs1];
+    ptr36_t phys;
+
+    rv_exc_t ex = rv_convert_addr(cpu, virt, &phys, true, true);
+
+    if(ex != rv_exc_none){
+        return ex;
+    }
+
+    if(phys != cpu->reserved_addr){
+        // target differs
+        alert("RV32IMA: LR/SC addresses do not match");
+        cpu->regs[instr.r.rs2] = 1;
+        return rv_exc_none;
+    }
+
+    // phys == reserved_addr
+    // here we suppose that only 32-bit reservations to aligned addresses are possible
+    // but since mips does not allow unaligned accesses, and only 32-bit ll is now supported in msim for mips
+    // and risc-v allows only aligned accesses (without Zam extension) and only 32-bit atomics are supported here
+    // this should be fine
+
+    ex = rv_write_mem32(cpu, virt, cpu->regs[instr.r.rs2], true);
+
+    if(ex != rv_exc_none){
+        alert("RV32IMA: SC write failed after successful address translation");
+        cpu->regs[instr.r.rs2] = 1;
+        return ex;
+    }
+
+    cpu->regs[instr.r.rs2] = 0;
     return rv_exc_none;
 }
 

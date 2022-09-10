9ec8a564db45761d742e325222217e0a1403be04
Jan Papesch <janpapuch@seznam.cz>
2022-09-10 15:56:40 +0200

    fixed interrupts

--- src/device/cpu/riscv_rv32ima/cpu.c
+++ src/device/cpu/riscv_rv32ima/cpu.c
@@ -14,7 +14,7 @@
 
 static void init_regs(rv_cpu_t *cpu) {
     ASSERT(cpu != NULL);
-    
+
     // expects that default value for any variable is 0
 
     cpu->pc = RV_START_ADDRESS;
@@ -160,8 +160,72 @@ void rv_cpu_set_pc(rv_cpu_t *cpu, uint32_t value){
     cpu->pc_next = value+4;    
 }
 
+static void handle_exception(rv_cpu_t* cpu, rv_exc_t ex){
+
+}
+
+static void try_handle_interrupt(rv_cpu_t* cpu){
+    // no interrupt pending
+    if(cpu->csr.mip == 0) return;
+
+
+
+}
+
+
+
+static void account_hmp(rv_cpu_t* cpu, int i){
+    ASSERT((i >= 0 && i < 29));
+    
+    uint32_t mask = (1 << i);
+    bool inhibited = cpu->csr.mcountinhibit & mask;
+    
+    if(inhibited) return;
+
+    csr_hpm_event_t event = cpu->csr.hpmevents[i];
+
+    switch(event){
+        case(hpm_u_cycles):{
+            if(cpu->priv_mode == rv_umode){
+                cpu->csr.hpmcounters[i]++;
+            }
+            break;
+        }
+        case(hpm_s_cycles):{
+            if(cpu->priv_mode == rv_smode){
+                cpu->csr.hpmcounters[i]++;
+            }
+            break;
+        }
+        case(hpm_m_cycles):{
+            if(cpu->priv_mode == rv_mmode){
+                cpu->csr.hpmcounters[i]++;
+            }
+            break;
+        }
+        case(hpm_w_cycles):{
+            // TODO: count stalled cycles (probably when the current instruction is WFI or cpu->stdby is true)
+            break;
+        }
+        default:
+            break;
+    }
+}
+
+static void account(rv_cpu_t* cpu){
+    cpu->csr.cycle++;
+    cpu->csr.instret++;
+
+    for(int i = 0; i < 29; ++i){
+        account_hmp(cpu, i);
+    }
+}
+
 void rv_cpu_step(rv_cpu_t *cpu){
     ASSERT(cpu != NULL);
+
+    // If any interrupts are pending, handle them
+    try_handle_interrupt(cpu);
 
     ptr36_t phys;
     rv_exc_t ex;
@@ -178,11 +242,11 @@ void rv_cpu_step(rv_cpu_t *cpu){
     
     ex = instr_func(cpu, instr_data);
 
+    account(cpu);
+
     if(ex != rv_exc_none){
-
+        handle_exception(cpu, ex);
     }
-
-    cpu->csr.cycle++;
 
     // x0 is always 0
     cpu->regs[0] = 0;
@@ -200,16 +264,33 @@ bool rv_sc_access(rv_cpu_t *cpu, ptr36_t phys){
         cpu->reserved_valid = false;
     }
     return hit;
-}
+}    
+
+
+
 
 /* Interrupts
- * This is supposed to be used with devices and inter-processor communication,
- * so this will raise/clear the Machine External Interrupt bit and ignore the no
+ * This is supposed to be used with devices and interprocessor communication,
+ * devices should raise a Machine/Supervisor External Interrupt,
+ * but interprocessor interrupts should be Machine/Supervisor Software Interrupts.
+ * So we use the argument no to differentiate based on the exception code (see rv_exc_t definiton)
  */
 
 void rv_interrupt_up(rv_cpu_t *cpu, unsigned int no){
     ASSERT(cpu != NULL);
-    cpu->csr.mip |= rv_csr_mei_mask;
+
+    // default to MEI if no is invalid
+    if( no != rv_exc_supervisor_software_interrupt &&
+        no != rv_exc_machine_software_interrupt &&
+        no != rv_exc_supervisor_external_interrupt &&
+        no != rv_exc_machine_external_interrupt
+    ){
+        no = rv_exc_machine_external_interrupt;
+    }
+
+    uint32_t mask = RV_EXCEPTION_MASK(no);
+
+    cpu->csr.mip |= mask;
 }
 
 void rv_interrupt_down(rv_cpu_t *cpu, unsigned int no){
@@ -217,5 +298,17 @@ void rv_interrupt_down(rv_cpu_t *cpu, unsigned int no){
     //! for simplicity just clears the bit
     //! if this interrupt could be raised by different means,
     //! this would not work!
-    cpu->csr.mip &= ~rv_csr_mei_mask;
+    
+    // default to MEI if no is invalid
+    if( no != rv_exc_supervisor_software_interrupt &&
+        no != rv_exc_machine_software_interrupt &&
+        no != rv_exc_supervisor_external_interrupt &&
+        no != rv_exc_machine_external_interrupt
+    ){
+        no = rv_exc_machine_external_interrupt;
+    }
+
+    uint32_t mask = RV_EXCEPTION_MASK(no);
+
+    cpu->csr.mip &= ~mask;
 }

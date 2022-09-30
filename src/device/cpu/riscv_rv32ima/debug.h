5940b6ce0262e2d28ec1774cd6b7279beed55fd6
Jan Papesch <janpapuch@seznam.cz>
2022-09-30 14:25:39 +0200

    dumpins

--- src/device/cpu/riscv_rv32ima/debug.h
+++ src/device/cpu/riscv_rv32ima/debug.h
@@ -24,6 +24,7 @@ extern bool rv_debug_change_regnames(rv_regname_type_t type);
 
 extern void rv_reg_dump(rv_cpu_t *cpu);
 extern void rv_idump(rv_cpu_t *cpu, uint32_t addr, rv_instr_t instr);
+extern void rv_idump_phys(uint32_t addr, rv_instr_t instr);
 extern void rv_csr_dump_all(rv_cpu_t *cpu);
 extern bool rv_csr_dump(rv_cpu_t *cpu, csr_num_t csr);
 extern bool rv_csr_dump_by_name(rv_cpu_t *cpu, const char* name);

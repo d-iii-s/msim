63dbacc9c9d078fb38d1f29219e857ddb52beeb8
Jan Papesch <janpapuch@seznam.cz>
2023-09-12 18:10:18 +0200

    fixed linking error

--- tests/rvtests/unit-tests/asid-len.c
+++ tests/rvtests/unit-tests/asid-len.c
@@ -8,14 +8,14 @@ PCUT_INIT
 
 PCUT_TEST_SUITE(asid_len);
 
-rv_cpu_t cpu;
+rv_cpu_t cpu0;
 
 PCUT_TEST_BEFORE {
-    rv_cpu_init(&cpu, 0);
+    rv_cpu_init(&cpu0, 0);
 }
 
 PCUT_TEST(default_value){
-    PCUT_ASSERT_INT_EQUALS(rv_asid_len, cpu.csr.asid_len);
+    PCUT_ASSERT_INT_EQUALS(rv_asid_len, cpu0.csr.asid_len);
 }
 
 PCUT_TEST(shorter_asid_zeroes_out){
@@ -23,15 +23,15 @@ PCUT_TEST(shorter_asid_zeroes_out){
     uint32_t ppn = 0x12345;
 
     // set asid to max value of 511 
-    cpu.csr.satp = rv_csr_satp_mode_mask | rv_csr_asid_mask | ppn;
+    cpu0.csr.satp = rv_csr_satp_mode_mask | rv_csr_asid_mask | ppn;
 
-    rv_csr_set_asid_len(&cpu, 7);
+    rv_csr_set_asid_len(&cpu0, 7);
 
     unsigned expected_asid = 0x7F;
     
-    uint32_t mode_after = cpu.csr.satp & rv_csr_satp_mode_mask;
-    unsigned asid_after = (cpu.csr.satp & rv_csr_asid_mask) >> rv_csr_satp_asid_offset;
-    uint32_t ppn_after = cpu.csr.satp & rv_csr_satp_ppn_mask;
+    uint32_t mode_after = cpu0.csr.satp & rv_csr_satp_mode_mask;
+    unsigned asid_after = (cpu0.csr.satp & rv_csr_asid_mask) >> rv_csr_satp_asid_offset;
+    uint32_t ppn_after = cpu0.csr.satp & rv_csr_satp_ppn_mask;
 
     PCUT_ASSERT_INT_EQUALS(expected_asid, asid_after);
     PCUT_ASSERT_INT_EQUALS(rv_csr_satp_mode_mask, mode_after);
@@ -40,19 +40,19 @@ PCUT_TEST(shorter_asid_zeroes_out){
 
 PCUT_TEST(longer_asid_perserves){
 
-    rv_csr_set_asid_len(&cpu, 7);
+    rv_csr_set_asid_len(&cpu0, 7);
 
     uint32_t ppn = 0x12345;
     unsigned asid = 0x7F;
 
     // set asid to max value of 511 
-    cpu.csr.satp = rv_csr_satp_mode_mask | (asid << rv_csr_satp_asid_offset) | ppn;
+    cpu0.csr.satp = rv_csr_satp_mode_mask | (asid << rv_csr_satp_asid_offset) | ppn;
 
-    rv_csr_set_asid_len(&cpu, 9);
+    rv_csr_set_asid_len(&cpu0, 9);
 
-    uint32_t mode_after = cpu.csr.satp & rv_csr_satp_mode_mask;
-    unsigned asid_after = (cpu.csr.satp & rv_csr_asid_mask) >> rv_csr_satp_asid_offset;
-    uint32_t ppn_after = cpu.csr.satp & rv_csr_satp_ppn_mask;
+    uint32_t mode_after = cpu0.csr.satp & rv_csr_satp_mode_mask;
+    unsigned asid_after = (cpu0.csr.satp & rv_csr_asid_mask) >> rv_csr_satp_asid_offset;
+    uint32_t ppn_after = cpu0.csr.satp & rv_csr_satp_ppn_mask;
 
     PCUT_ASSERT_INT_EQUALS(asid, asid_after);
     PCUT_ASSERT_INT_EQUALS(rv_csr_satp_mode_mask, mode_after);
@@ -60,7 +60,7 @@ PCUT_TEST(longer_asid_perserves){
 }
 
 PCUT_TEST(csr_write_masks){
-    rv_csr_set_asid_len(&cpu, 7);
+    rv_csr_set_asid_len(&cpu0, 7);
 
     rv_instr_t instr = { .i = {
         .opcode = rv_opcSYSTEM,
@@ -69,27 +69,27 @@ PCUT_TEST(csr_write_masks){
         .rs1 = 1,
         .rd  = 2
     }};
-    cpu.priv_mode = rv_mmode;
+    cpu0.priv_mode = rv_mmode;
 
     // Write max asid (0x1FF)
-    cpu.regs[instr.i.rs1] = rv_csr_satp_mode_mask | rv_csr_asid_mask;
-    cpu.regs[instr.i.rd] = 0;
+    cpu0.regs[instr.i.rs1] = rv_csr_satp_mode_mask | rv_csr_asid_mask;
+    cpu0.regs[instr.i.rd] = 0;
 
-    cpu.csr.satp = 0;
+    cpu0.csr.satp = 0;
 
-    rv_exc_t ex = rv_csrrw_instr(&cpu, instr);
+    rv_exc_t ex = rv_csrrw_instr(&cpu0, instr);
 
     PCUT_ASSERT_INT_EQUALS(rv_exc_none, ex);
 
     // Only 7 set bits
     unsigned expected_asid = 0x7F;
-    unsigned asid_after = (cpu.csr.satp & rv_csr_asid_mask) >> rv_csr_satp_asid_offset;
+    unsigned asid_after = (cpu0.csr.satp & rv_csr_asid_mask) >> rv_csr_satp_asid_offset;
 
     PCUT_ASSERT_INT_EQUALS(expected_asid, asid_after);
 }
 
 PCUT_TEST(csr_set_masks){
-    rv_csr_set_asid_len(&cpu, 7);
+    rv_csr_set_asid_len(&cpu0, 7);
 
     rv_instr_t instr = { .i = {
         .opcode = rv_opcSYSTEM,
@@ -98,21 +98,21 @@ PCUT_TEST(csr_set_masks){
         .rs1 = 1,
         .rd  = 2
     }};
-    cpu.priv_mode = rv_mmode;
+    cpu0.priv_mode = rv_mmode;
 
     // Write max asid (0x1FF)
-    cpu.regs[instr.i.rs1] = rv_csr_satp_mode_mask | rv_csr_asid_mask;
-    cpu.regs[instr.i.rd] = 0;
+    cpu0.regs[instr.i.rs1] = rv_csr_satp_mode_mask | rv_csr_asid_mask;
+    cpu0.regs[instr.i.rd] = 0;
 
-    cpu.csr.satp = 0;
+    cpu0.csr.satp = 0;
 
-    rv_exc_t ex = rv_csrrs_instr(&cpu, instr);
+    rv_exc_t ex = rv_csrrs_instr(&cpu0, instr);
 
     PCUT_ASSERT_INT_EQUALS(rv_exc_none, ex);
 
     // Only 7 set bits
     unsigned expected_asid = 0x7F;
-    unsigned asid_after = (cpu.csr.satp & rv_csr_asid_mask) >> rv_csr_satp_asid_offset;
+    unsigned asid_after = (cpu0.csr.satp & rv_csr_asid_mask) >> rv_csr_satp_asid_offset;
 
     PCUT_ASSERT_INT_EQUALS(expected_asid, asid_after);
 }
@@ -129,21 +129,21 @@ static unsigned probe_asid_len(){
         .rd  = 2
     }};
 
-    cpu.regs[instr.i.rs1] = 0xFFFFFFFF;
-    cpu.regs[instr.i.rd] = 0;
+    cpu0.regs[instr.i.rs1] = 0xFFFFFFFF;
+    cpu0.regs[instr.i.rd] = 0;
 
-    rv_exc_t ex = rv_csrrs_instr(&cpu, instr);
+    rv_exc_t ex = rv_csrrs_instr(&cpu0, instr);
     PCUT_ASSERT_INT_EQUALS(rv_exc_none, ex);
 
     // Read the content of SATP
 
-    cpu.regs[instr.i.rs1] = 0;
-    cpu.regs[instr.i.rd] = 0;
+    cpu0.regs[instr.i.rs1] = 0;
+    cpu0.regs[instr.i.rd] = 0;
 
-    ex = rv_csrrs_instr(&cpu, instr);
+    ex = rv_csrrs_instr(&cpu0, instr);
     PCUT_ASSERT_INT_EQUALS(rv_exc_none, ex);
 
-    uint32_t satp_content = cpu.regs[instr.i.rd];
+    uint32_t satp_content = cpu0.regs[instr.i.rd];
 
     unsigned asid_mask = (satp_content & rv_csr_asid_mask) >> rv_csr_satp_asid_offset;
 
@@ -158,10 +158,10 @@ static unsigned probe_asid_len(){
 }
 
 static void test_asid_len_probe(unsigned asid_len){
-    cpu.priv_mode = rv_mmode;
-    cpu.csr.satp = 0;
+    cpu0.priv_mode = rv_mmode;
+    cpu0.csr.satp = 0;
 
-    rv_csr_set_asid_len(&cpu, asid_len);
+    rv_csr_set_asid_len(&cpu0, asid_len);
 
     unsigned probed_asid_len = probe_asid_len();
 

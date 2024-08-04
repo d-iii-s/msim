c61c49d0988464af0832d046c623dc1db6f4456f
Jan Papesch <janpapuch@seznam.cz>
2024-08-04 10:33:34 +0200

    clang-format

--- src/device/cpu/riscv_rv32ima/debug.c
+++ src/device/cpu/riscv_rv32ima/debug.c
@@ -745,7 +745,7 @@ static void rv_pte_addr_dump(ptr36_t pte_addr, ptr36_t pt_base_addr, uint32_t vp
     printf(RV_DEBUG_INDENT "This entry ^ physical address: 0x%09lx = 0x%09lx + 0x%03x * %d\n", pte_addr, pt_base_addr, vpn_i, RV_PTESIZE);
 }
 
-static void rv_pte_translation_step_dump(const char* header, sv32_pte_t pte, ptr36_t pte_addr, ptr36_t pt_base_addr, uint32_t vpn_i)
+static void rv_pte_translation_step_dump(const char *header, sv32_pte_t pte, ptr36_t pte_addr, ptr36_t pt_base_addr, uint32_t vpn_i)
 {
     printf("%s ", header);
     rv_pte_dump(pte);

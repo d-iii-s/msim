0cd2e5e1f75fe3ff28dbc5262771798ae89302f5
Petr Hrdina <phrdina@proton.me>
2025-02-24 07:39:26 +0100

    NIC: basic commands WIP

--- src/device/dnetcard.c
+++ src/device/dnetcard.c
@@ -11,7 +11,47 @@
 
 #include <stdio.h>
 
+#include "../fault.h"
+#include "../text.h"
+#include "../utils.h"
 #include "dnetcard.h"
+
+/** Actions the network card is performing */
+enum action_e {
+    ACTION_NONE, /**< Network card is idle */
+    ACTION_SEND, /**< Network card is sending a packet */
+};
+
+/* Register offsets */
+#define REGISTER_ADDR_LO 0 /* Address (bits 0 .. 31) */
+#define REGISTER_ADDR_HI 4 /* Address (bits 32 .. 35) */
+#define REGISTER_STATUS 8 /* Status/commands */
+#define REGISTER_COMMAND 8 /* Status/commands */
+#define REGISTER_LIMIT 12 /* Size of register block */
+
+/* Status flags */
+#define STATUS_MASK 0x0c /* Status mask */
+
+/* Command flags */
+#define COMMAND_SEND 0x01 /**< Send packet */
+#define COMMAND_MASK 0x07 /* Command mask */
+
+/** Network card instance data structure */
+typedef struct {
+    uint32_t *txbuffer; /* Transfer buffer */
+
+    /* Configuration */
+    ptr36_t addr; /* Register address */
+    unsigned int intno; /* Interrupt number */
+
+    /* Registers */
+    ptr36_t netcard_ptr; /* Current DMA pointer */
+    uint32_t netcard_status; /* Network card status register */
+    uint32_t netcard_command; /* Network card command register */
+
+    /* Current action variables */
+    enum action_e action; /* Action type */
+} netcard_data_s;
 
 /** Init command implementation
  *
@@ -23,6 +63,46 @@
  */
 static bool dnetcard_init(token_t *parm, device_t *dev)
 {
+    parm_next(&parm);
+    uint64_t _addr = parm_uint_next(&parm);
+    uint64_t _intno = parm_uint_next(&parm);
+
+    if (!phys_range(_addr)) {
+        error("Physical memory address out of range");
+        return false;
+    }
+
+    if (!phys_range(_addr + (uint64_t) REGISTER_LIMIT)) {
+        error("Invalid address, registers would exceed the physical "
+              "memory range");
+        return false;
+    }
+
+    ptr36_t addr = _addr;
+
+    if (!ptr36_dword_aligned(addr)) {
+        error("Physical memory address must be 8-byte aligned");
+        return false;
+    }
+
+    if (_intno > MAX_INTRS) {
+        error("%s", txt_intnum_range);
+        return false;
+    }
+
+    /* Allocate structure */
+    netcard_data_s *data = safe_malloc_t(netcard_data_s);
+    dev->data = data;
+
+    /* Initialization */
+    data->addr = addr;
+    data->intno = _intno;
+    data->txbuffer = safe_malloc(1); /* todo size should be max ip packet size */
+    data->netcard_ptr = 0;
+    data->netcard_status = 0;
+    data->netcard_command = 0;
+    data->action = ACTION_NONE;
+
     return true;
 }
 
@@ -61,6 +141,7 @@ static bool dnetcard_stat(token_t *parm, device_t *dev)
  */
 static void dnetcard_done(device_t *dev)
 {
+    // todo free txbuffer
 }
 
 /** Read command implementation
@@ -73,6 +154,20 @@ static void dnetcard_done(device_t *dev)
 static void dnetcard_read32(unsigned int procno, device_t *dev, ptr36_t addr,
         uint32_t *val)
 {
+    netcard_data_s *data = (netcard_data_s *) dev->data;
+
+    /* Read internal registers */
+    switch (addr - data->addr) {
+    case REGISTER_ADDR_LO:
+        *val = (uint32_t) (data->netcard_ptr & UINT32_C(0xffffffff));
+        break;
+    case REGISTER_ADDR_HI:
+        *val = (uint32_t) (data->netcard_ptr >> 32);
+        break;
+    case REGISTER_STATUS:
+        *val = data->netcard_status;
+        break;
+    }
 }
 
 /** Write command implementation
@@ -85,6 +180,30 @@ static void dnetcard_read32(unsigned int procno, device_t *dev, ptr36_t addr,
 static void dnetcard_write32(unsigned int procno, device_t *dev, ptr36_t addr,
         uint32_t val)
 {
+    netcard_data_s *data = (netcard_data_s *) dev->data;
+
+    switch (addr - data->addr) {
+    case REGISTER_ADDR_LO:
+        data->netcard_ptr &= ~((ptr36_t) UINT32_C(0xffffffff));
+        data->netcard_ptr |= val;
+        break;
+    case REGISTER_ADDR_HI:
+        data->netcard_ptr &= (ptr36_t) UINT32_C(0xffffffff);
+        data->netcard_ptr |= ((ptr36_t) val) << 32;
+        break;
+
+    case REGISTER_COMMAND:
+        break;
+        /* Remove unused bits */
+        data->netcard_command = val & COMMAND_MASK;
+
+        if ((data->netcard_command & COMMAND_SEND) && data->action != ACTION_NONE) {
+            /* Command in progress */
+            // todo
+        }
+
+        // todo send command
+    }
 }
 
 /** Network card implementation

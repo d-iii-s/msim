b913a2d76194deb5fb8042e374e601278cf82674
Vojtech Horky <horky@d3s.mff.cuni.cz>
2026-09-07 13:32:45 +0200

    Unified tests: add kputs

--- tests/unified/src/shared/include/kernel.h
+++ tests/unified/src/shared/include/kernel.h
@@ -4,5 +4,14 @@
 extern void kernel_wrap(void);
 extern void kernel_main(void);
 
+static inline void kputs(const char *s) {
+    volatile char *printer = (volatile char *)0x90000000;
+    while (*s != 0) {
+        *printer = *s;
+        s++;
+    }
+    *printer = '\n';
+}
+
 #endif
 

44b76b645bc399c80512783b5759c720460b0072
Vojtech Horky <horky@d3s.mff.cuni.cz>
2026-09-17 14:41:00 +0200

    Tests: simplify path rewriting

--- tests/unified/common.bash
+++ tests/unified/common.bash
@@ -138,7 +138,7 @@ msim_run_code() {
 
     echo "quit" >>"$MSIM_TEST_TMPDIR/msim.conf"
     (
-        sed -e "s#\"boot.bin\"#\"$test_dir/boot.bin\"#" -e "s#\"kernel.bin\"#\"$test_dir/kernel.bin\"#" <"$test_dir/msim.conf"
+        sed -e 's#"\([A-Za-z0-9_.-]\+[.]bin\)"#"'"$test_dir"'/\1"#g' <"$test_dir/msim.conf"
         if grep -q printer "$test_dir/msim.conf"; then
             echo "printer redir \"$MSIM_TEST_TMPDIR/printer.output\""
         fi
